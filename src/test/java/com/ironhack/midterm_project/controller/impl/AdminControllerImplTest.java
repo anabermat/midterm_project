283fa55db771b91e969fd101928eb578d1a1bb99
Ana Bermejo Mata <bermeana17@gmail.com>
2022-07-05 09:56:27 +0200

    tests done

--- src/test/java/com/ironhack/midterm_project/controller/impl/AdminControllerImplTest.java
+++ src/test/java/com/ironhack/midterm_project/controller/impl/AdminControllerImplTest.java
@@ -3,8 +3,12 @@ package com.ironhack.midterm_project.controller.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ironhack.midterm_project.classes.Address;
 import com.ironhack.midterm_project.classes.Money;
+import com.ironhack.midterm_project.dto.AccountBalanceDTO;
 import com.ironhack.midterm_project.enums.Status;
 import com.ironhack.midterm_project.model.account.Account;
+import com.ironhack.midterm_project.model.account.Checking;
+import com.ironhack.midterm_project.model.account.CreditCard;
+import com.ironhack.midterm_project.model.account.Savings;
 import com.ironhack.midterm_project.model.user.AccountHolder;
 import com.ironhack.midterm_project.model.user.Admin;
 import com.ironhack.midterm_project.model.user.Role;
@@ -20,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -32,7 +37,7 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -136,32 +141,73 @@ class AdminControllerImplTest {
     void updateBalance() throws Exception{
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Authorization", "Basic YWRtaW46MTIzNDU2");
-
+        AccountBalanceDTO accountBalanceDTO = new AccountBalanceDTO();
+       accountBalanceDTO.setBalance(new Money(BigDecimal.valueOf(1500)));
+        String body = objectMapper.writeValueAsString(accountBalanceDTO);
         // Validamos que el estatus de respuesta sea OK
-        MvcResult mvcResult = mockMvc.perform(get("/AdminAccounts/1/balance").headers(httpHeaders))
+        MvcResult mvcResult = mockMvc.perform(
+                patch("/AdminAccounts/1/balance")
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .headers(httpHeaders))
                 .andExpect(status().isOk())
                 .andReturn();
 
-        assertTrue(mvcResult.getResponse().getContentAsString().contains("1250"));
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("1500"));
     }
 
     @Test
     void createCheckingAccount() throws Exception{
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Basic YWRtaW46MTIzNDU2");
+        Checking checking = new Checking(new Money(BigDecimal.valueOf(1520)), 1250, accountHolder1,accountHolder2,Status.ACTIVE,
+                new Date(2015/03/21));
+        String body = objectMapper.writeValueAsString(checking);
+        MvcResult mvcResult = mockMvc.perform(
+                        post("/AdminAccounts/CheckingAccount")
+                                .content(body)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .headers(httpHeaders))
+                .andExpect(status().isOk())
+                .andReturn();
 
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("1520"));
     }
 
     @Test
     void createSavingsAccount() throws Exception{
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Basic YWRtaW46MTIzNDU2");
+        Savings savings = new Savings(new Money(BigDecimal.valueOf(1520)), 1250, accountHolder1,accountHolder2,Status.ACTIVE,
+                new Date(2015/03/21));
+        String body = objectMapper.writeValueAsString(savings);
+        MvcResult mvcResult = mockMvc.perform(
+                        post("/AdminAccounts/SavingsAccount")
+                                .content(body)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .headers(httpHeaders))
+                .andExpect(status().isOk())
+                .andReturn();
 
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("1520"));
     }
 
     @Test
     void createCreditCard() throws Exception{
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Basic YWRtaW46MTIzNDU2");
+        CreditCard creditCard = new CreditCard( new Money(BigDecimal.valueOf(1520)), 1250, accountHolder1,
+                accountHolder2,Status.ACTIVE, new Money(BigDecimal.valueOf(950)), BigDecimal.valueOf(0.2));
+        String body = objectMapper.writeValueAsString(creditCard);
+        MvcResult mvcResult = mockMvc.perform(
+                        post("/AdminAccounts/CreditCard")
+                                .content(body)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .headers(httpHeaders))
+                .andExpect(status().isOk())
+                .andReturn();
 
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("1520"));
     }
 
-    @Test
-    void delete() throws Exception{
-        
-    }
 }
